8d723fffce1434588c1b0ac48261b3774c4f11c6
Roy3838 <roymedina@me.com>
2024-07-31 12:33:22 -0600

    correct wrong setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='chat_client',
     version='1.0.0',
-    packages=find_packages(),
+    py_modules=['main'],  # This tells setup to include 'main.py'
     include_package_data=True,
     install_requires=[
         'argparse',
     ],
     entry_points={
         'console_scripts': [
-            'chat_client=chat_client:main',
+            'chat_client=main:main',
         ],
     },
 )
-
